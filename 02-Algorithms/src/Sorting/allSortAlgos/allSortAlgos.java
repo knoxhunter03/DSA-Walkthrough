65b419a3dfd1445977aef38dab3e2d560750f8ad
knoxhunter03 <mangalamkallo03@gmail.com>
2022-03-19 23:57:50 +0530

    Add mergeSort to Revision

--- 02-Algorithms/src/Sorting/allSortAlgos/allSortAlgos.java
+++ 02-Algorithms/src/Sorting/allSortAlgos/allSortAlgos.java
@@ -33,6 +33,60 @@ public class allSortAlgos {
         }
     }
 
+    private static void mergeSort3(int[] inputArray){
+        int inputSize = inputArray.length;
+        int midIndex = inputSize / 2;
+        
+        int[] leftHalf = new int[midIndex];
+        int[] rightHalf = new int[inputSize - midIndex];
+
+        for(int i = 0; i < midIndex; i++){
+            leftHalf[i] = inputArray[i];
+        }
+
+        for(int i = midIndex; i < inputSize; i++){
+            rightHalf[i - midIndex] = inputArray[i];
+        }
+
+        mergeSort3(leftHalf);
+        mergeSort3(rightHalf);
+
+        merge(inputArray, leftHalf, rightHalf);
+
+
+    }
+
+    public static void merge(int[] inputArray, int[] leftHalf, int[] righHalf){
+        int leftSize = leftHalf.length;
+        int rightSize = righHalf.length;
+
+        int i = 0;
+        int j = 0;
+        int k = 0;
+
+        while (i < leftSize && j < rightSize){
+
+            if(leftHalf[i] <= righHalf[i]){
+                inputArray[k] = leftHalf[i];
+                i++;
+            }
+            else{
+                inputArray[k] = righHalf[j];
+                k++;
+            }
+        }
+        while (i < leftSize){
+            inputArray[k] = leftHalf[i];
+            i++;
+            k++;
+        }
+        while (j < rightSize){
+            inputArray[k] = righHalf[j];
+            j++;
+            k++;
+        }
+    }
+
     public static void main(String[] args) {
         System.out.println("****** Given a set of Integers ******");
         int[] a = {9, 8, 7, 6, 5, 4, 3, 2, 1};
@@ -55,6 +109,10 @@ public class allSortAlgos {
 
             case 2:
                 inssertionSort2(a);
+                break;
+
+            case 3:
+                mergeSort3(a);
                 break;
 
             default:
