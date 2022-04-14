979fcb0c405588b47aaf554d0f86e1a317ebae2e
knoxhunter03 <mangalamkallo03@gmail.com>
2022-04-14 23:28:08 +0530

    Update test2.java

---
+++ 03-Questions/src/Testing/test2.java
@@ -0,0 +1,17 @@
+import java.util.Stack;
+
+class test2{
+
+    class Pair{
+        long first;
+        long second;
+
+        Pair(long a, long b){
+            this.first = a;
+        }
+    }
+
+    public static void main(String[] args) {
+        Stack<Pair> c = new Stack<>();
+    }
+}
